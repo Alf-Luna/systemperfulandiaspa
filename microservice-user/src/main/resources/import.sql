eef7ae1283ba206957ece9cc466ec781b860fff5
Alfo <alf.luna@duocuc.cl>
2025-06-06 23:07:21 -0400

    added imports for sql, modified data consistency

---
+++ microservice-user/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER (NAME, PHONE_NUMBER, EMAIL) VALUES ('Alfonso Luna', '987654321', 'alfo.luna@ejemplo.com');
+INSERT INTO USER (NAME, PHONE_NUMBER, EMAIL) VALUES ('Felipe Becerra', '912345678', 'felipe.becerra@ejemplo.com');
+INSERT INTO USER (NAME, PHONE_NUMBER, EMAIL) VALUES ('Alejandro Placencia', '998877665', 'alejandro.placencia@ejemplo.com');
