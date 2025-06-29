07c9a0f6761c4e7db33d6536f8b78e3e38f0150e
Alejandro <placencia.menares@gmail.com>
2025-06-28 20:46:56 -0400

    creacion hateoas microservicio ventas

---
+++ microservice-sale/src/main/java/com/tecnotrans/microservice_sale/Controller/SaleControllerV2.java
@@ -0,0 +1,5 @@
+package com.tecnotrans.microservice_sale.Controller;
+
+public class SaleControllerV2 {
+
+}
---
+++ microservice-sale/src/main/java/com/tecnotrans/microservice_sale/assemblers/SaleModelAssembler.java
@@ -0,0 +1,5 @@
+package com.tecnotrans.microservice_sale.assemblers;
+
+public class SaleModelAssembler {
+
+}
